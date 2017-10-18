c4d9327e855177e574727f82cce7a3353c1d0d8b
Jason Lam <jaylam111@gmail.com>
2017-10-18 09:31:21 -0400

    face demo

--- public/scripts/deuce.js
+++ public/scripts/deuce.js
@@ -27,10 +27,21 @@ class Ball extends Rect
 const canvas = document.getElementById('deuce');
 const context = canvas.getContext('2d');
 
-const eyeBall = new Ball;
-// console.log('ball : ', ball)
-eyeBall.position.x = 20;
-eyeBall.position.y = 30;
+const eyeBallL = new Ball;
+console.log('eyeBallL : ', eyeBallL)
+eyeBallL.position.x = 28;
+eyeBallL.position.y = 35;
+
+const eyeBallR = new Ball;
+eyeBallR.position.x = eyeBallL.position.x * 5;
+eyeBallR.position.y = eyeBallL.position.y;
+
+const mouth = new Rect;
+console.log('mouth : ', mouth)
+mouth.position.x = 15;
+mouth.position.y = 125;
+mouth.size.x = 150;
+mouth.size.y = 20;
 
 
 // main canvas
@@ -39,5 +50,9 @@ context.fillRect(0, 0, canvas.width, canvas.height);
 
 // ball
 context.fillStyle = '#f00';
-context.fillRect(eyeBall.position.x, eyeBall.position.y, eyeBall.size.x, eyeBall.size.y);
+context.fillRect(eyeBallL.position.x, eyeBallL.position.y, eyeBallL.size.x, eyeBallL.size.y);
+context.fillStyle = '#f00';
+context.fillRect(eyeBallR.position.x, eyeBallR.position.y, eyeBallR.size.x, eyeBallR.size.y);
 
+context.fillStyle = '#000';
+context.fillRect(mouth.position.x, mouth.position.y,mouth.size.x,mouth.size.y);
